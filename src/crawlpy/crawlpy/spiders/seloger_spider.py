3c6fc028871f8308aa853a1c9b506d270d0d0b91
dzlab <dzlabs@outlook.com>
2015-05-02 11:22:58 +0200

    replacing deprecated HtmlXPathSelector~

--- src/crawlpy/crawlpy/spiders/seloger_spider.py
+++ src/crawlpy/crawlpy/spiders/seloger_spider.py
@@ -3,7 +3,7 @@ __author__ = 'dzlab'
 import re
 
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.selector import HtmlXPathSelector
+from scrapy.selector import Selector
 from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
 
 class SelogerSpider(CrawlSpider):
@@ -27,26 +27,28 @@ class SelogerSpider(CrawlSpider):
 
     # http://doc.scrapy.org/en/0.18/intro/tutorial.html#intro-tutorial
     def parse_article(self, response):
-        hxs = HtmlXPathSelector(response)
-        listing = hxs.select('//div[contains(@class, "main listing")]')
+        hxs = Selector(response=response)
+        # parse reference
+        ref  = hxs.xpath('//span[contains(@class, "description_ref")]/text()').extract()[0].split()[1]
+        listing = hxs.xpath('//div[contains(@class, "main listing")]')
         # parse title
-        title = listing.select('./div/div/h1/text()').extract()
+        title = listing.xpath('./div/div/h1/text()').extract()
         title = " ".join(title[0].encode("utf-8").split())
         # parse price
-        price = listing.select('./div/section/div/div/div/span/text()').extract() #[contains(@class, "resume__prix"]
+        price = listing.xpath('./div/section/div/div/div/span/text()').extract() #[contains(@class, "resume__prix"]
         price = " ".join(price[0].encode("utf-8").split())
         # parse location
-        location = hxs.select('//h2[contains(@class, "detail-subtitle")]/span/text()').extract()
+        location = hxs.xpath('//h2[contains(@class, "detail-subtitle")]/span/text()').extract()
         location = re.sub(r'[\xe0]', r' ', location[0])
         location = " ".join(location.encode("utf-8").split())
         # parse the description section: surface, floor, etc.
-        description = hxs.select('//ol[contains(@class, "description-liste")]/li/text()').extract()
+        description = hxs.xpath('//ol[contains(@class, "description-liste")]/li/text()').extract()
         surface = " ".join(description[0].encode("utf-8").replace("Surface de", "").split())
         floor = " ".join(description[1].encode("utf-8").replace("Surface de", "").split())
         room = " ".join(description[2].encode("utf-8").replace("Surface de", "").split())
-
+        # parse fees
         fees = None
-        elms = hxs.select('//ol[contains(@class, "description-liste")]/li/a/text()').extract()
+        elms = hxs.xpath('//ol[contains(@class, "description-liste")]/li/a/text()').extract()
         if len(elms) > 0:
             for i in range(len(elms)):
                 if "Honoraires ttc :" in elms[i].encode("utf-8"):
